bde4000c5a3a09cac3c81e869f78938907fe8586
moonisme <37928210+moonisme@users.noreply.github.com>
2019-05-30 15:44:58 +1000

    Add files via upload

---
+++ Double_Linked_List/cmake-build-debug/CMakeFiles/ass.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass.dir/main.c.obj"
+  "ass.pdb"
+  "ass.exe"
+  "ass.exe.manifest"
+  "libass.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
